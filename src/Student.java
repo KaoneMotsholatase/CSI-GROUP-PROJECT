5d5f3d5287dbb3e62207b735d69c03691da544d8
priscillahkgotlayame <201702204@ub.ac.bw>
2025-04-22 16:52:56 +0200

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -85,20 +85,4 @@ public class Student {
          }     
    }  
 
-   //sort based on Student ID
-    public static void selectionSort(Student[] arr) {
-         for (int i = 0; i < arr.length - 1; i++) {
-             int minIndex = i;
-             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j].getStudentID() < arr[minIndex].getStudentID()) {
-              minIndex = j;
-                }
-            }
-            
-            Student temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-         }     
-   }  
-
 }
