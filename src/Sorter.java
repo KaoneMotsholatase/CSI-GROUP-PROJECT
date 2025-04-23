f8d8a3ce64527bf45c430d3eb33f418295d6bbfd
priscillahkgotlayame <201702204@ub.ac.bw>
2025-04-23 14:47:53 +0200

    Added insertionSort based on GPA

--- src/Sorter.java
+++ src/Sorter.java
@@ -15,5 +15,18 @@ public class Sorter{
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
          }     
-   }  
+   } 
+  
+  //sort based on GPA
+  public static void insertionSort(Student[] arr, int size) {
+        for (int i = 1; i < size; i++) {
+            Student key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].getGpa() > key.getGpa()) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
