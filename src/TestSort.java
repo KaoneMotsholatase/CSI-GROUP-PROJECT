1297d642fd49336f7a2af96f0041f18d3c2f795a
KaoneMotsholatase <kaonemotsholatase@gmail.com>
2025-04-23 16:14:54 +0200

    Update TestSort.java

--- src/TestSort.java
+++ src/TestSort.java
@@ -20,4 +20,19 @@ public class TestSort {
             System.out.println(s.getStudentID() + " - " + s.getName());
         }
     }
-}
+ // Resetting the array to its original state for insertion sort
+        students = new Student[] {
+            new Student(102, "Mia", 3.4),
+            new Student(100, "Katlego", 3.9),
+            new Student(101, "Aneesah", 2.8)
+        };
+
+        // Sort students by GPA using insertionSort method
+        Sorter.insertionSort(students, students.length);
+
+        // Display students after sorting by GPA using insertionSort
+        System.out.println("\nAfter Sorting by GPA (Insertion Sort):");
+        for (Student s : students) {
+            System.out.println(s.getStudentID() + " - " + s.getName() + " - " + s.getGpa());
+        }
+    }
