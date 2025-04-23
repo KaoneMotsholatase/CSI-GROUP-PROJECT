977d713e5f7b549e1e4a2f1ccc2d869b11a0b8f7
KaoneMotsholatase <kaonemotsholatase@gmail.com>
2025-04-23 16:59:43 +0200

    Update TestSort.java

--- src/TestSort.java
+++ src/TestSort.java
@@ -7,32 +7,33 @@ public class TestSort {
             new Student(101, "Aneesah", 2.8)
         };
 
-        System.out.println("Before Sorting:");
+        System.out.println("Before Sorting by Student ID:");
         for (Student s : students) {
             System.out.println(s.getStudentID() + " - " + s.getName());
         }
 
-        // Sort students by ID
+        // Sorting students by ID using selectionSort
         Sorter.selectionSort(students);
 
+        
         System.out.println("\nAfter Sorting by Student ID:");
         for (Student s : students) {
             System.out.println(s.getStudentID() + " - " + s.getName());
         }
-    }
- // Resetting the array to its original state for insertion sort
+
+        // Resetting the array to its original state for GPA sorting
         students = new Student[] {
             new Student(102, "Mia", 3.4),
             new Student(100, "Katlego", 3.9),
             new Student(101, "Aneesah", 2.8)
         };
 
-        // Sort students by GPA using insertionSort method
-        Sorter.insertionSort(students, students.length);
+        // Sorting students by GPA using insertionSort
+        Sorter.insertionSort(students);
 
-        // Display students after sorting by GPA using insertionSort
         System.out.println("\nAfter Sorting by GPA (Insertion Sort):");
         for (Student s : students) {
-            System.out.println(s.getStudentID() + " - " + s.getName() + " - " + s.getGpa());
+            System.out.println(s.getStudentID() + " - " + s.getName() + " - GPA: " + s.getGPA());
         }
     }
+}
